ecbd12a7082f7ae9717956ef02cf46a6e57eecb0
lzeeorno <fuchen1@ualberta.ca>
2019-10-11 19:18:57 -0600

    as4

---
+++ as4/nomenclator.py
@@ -0,0 +1,48 @@
+import random
+
+def encryptMessage(subKey, codeBook, message):
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    wordList = message.split(' ')
+    for i in range(len(wordList)):
+        transWord=''
+        if wordList[i].upper() in codeBook:
+            wordList[i] = random.choice(codeBook[wordList[i].upper()])
+        elif wordList[i].lower() in codeBook:
+            wordList[i] = random.choice(codeBook[wordList[i].lower()])
+        elif wordList[i].title() in codeBook:
+            wordList[i] = random.choice(codeBook[wordList[i].title()])
+        else:
+            for symbol in wordList[i]:
+                if symbol.upper() in letters:
+                    symIndex = letters.find(symbol.upper())
+                    if symbol.isupper():
+                        transWord += subKey[symIndex].upper()
+                    else:
+                        transWord += subKey[symIndex].lower()
+                else:
+                    transWord += symbol
+            wordList[i] = transWord
+    return ' '.join(wordList)
+                        
+                    
+def decryptMessage(subKey, codeBook, message):
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    wordList = message.split(' ')
+    for i in range(len(wordList)):
+        transWord=''
+        if wordList[i].replace(',','').replace("'",'').replace('.','').isalpha():
+            for symbol in wordList[i]:
+                if symbol.upper() in subKey:
+                    symIndex = subKey.find(symbol.upper())
+                    if symbol.isupper():
+                        transWord += letters[symIndex].upper()
+                    else:
+                        transWord += letters[symIndex].lower()
+                else:
+                    transWord += symbol
+            wordList[i] = transWord
+        else:
+            for key in codeBook:
+                if wordList[i] in codeBook.get(key):
+                    wordList[i] = key
+    return ' '.join(wordList)
