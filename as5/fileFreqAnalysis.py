aca9a17e388adf30c5b4a44c318fba0ae7b821e7
xdai2 <43150995+xdai2@users.noreply.github.com>
2019-10-17 04:41:32 +0800

    hbj

---
+++ as5/fileFreqAnalysis.py
@@ -0,0 +1,51 @@
+
+ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def freqDict(f1, frequency):
+    letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+    op = open(f1, encoding = 'UTF-8')
+    cipher = op.read()
+    op.close()
+    for letter in cipher:
+        if letter in LETTERS:
+            letterCount[letter] += 1
+    return letterCount
+
+def freqDecrypt(f1, f2, frequency):
+    mapping = freqDict(f1, frequency)
+    
+    freqToLetter = {}
+    for letter in LETTERS:
+        if mapping[letter] not in freqToLetter:
+            freqToLetter[mapping[letter]] = [letter]
+        else:
+            freqToLetter[mapping[letter]].append(letter)
+
+    for freq in freqToLetter:
+        freqToLetter[freq].sort(key=frequency.find, reverse=True)
+        freqToLetter[freq] = ''.join(freqToLetter[freq])
+        
+    freqPairs = list(freqToLetter.items())
+    freqPairs.sort(key=takeFirst, reverse=True)
+    
+    freqOrder = []
+    for freqPair in freqPairs:
+        freqOrder.append(freqPair[1])
+    freq = ''.join(freqOrder)
+    op = open(f1, encoding='UTF-8')
+    cipher = op.read()
+    op.close()
+    final = ''
+    for letter in cipher:
+        if letter in freq:
+            final += frequency[freq.find(letter)]
+        else:
+            final += letter
+    op = open(f2, 'w')
+    op.write(final)
+    op.close()
+
+def takeFirst(item):
+    return item[0]
+        
---
+++ as5/subEval.py
@@ -0,0 +1,42 @@
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def evalFile(f1, f2):
+    op = open(f1, encoding='UTF-8',errors='ignore')
+    plain = op.read()
+    op.close()
+    op = open(f2, encoding = 'UTF-8')
+    imperfect = op.read()
+    op.close()
+
+    list1 = []
+    list2 = []
+    for letter in plain:
+        if letter.upper() in LETTERS:
+            if letter.upper() not in list1:
+                list1.append(letter.upper())
+    for letter in imperfect:
+        if letter.upper() in LETTERS:
+            if letter.upper() not in list2:
+                list2.append(letter.upper())
+    keyAccuaracy = 0
+    for i in range(len(list1)):
+        if list1[i] == list2[i]:
+            keyAccuaracy += 1
+    keyAccuaracy = keyAccuaracy/len(list1)
+
+    list1 = []
+    list2 = []
+    for letter in plain:
+        if letter.upper() in LETTERS:
+            list1.append(letter.upper())
+    for letter in imperfect:
+        if letter.upper() in LETTERS:
+            list2.append(letter.upper())
+
+    deciphermentAccuracy = 0
+    
+    for i in range(len(list2)):
+        if list1[i] == list2[i]:
+            deciphermentAccuracy += 1
+    deciphermentAccuracy = deciphermentAccuracy/len(list1)
+
+    return [keyAccuaracy, deciphermentAccuracy]
